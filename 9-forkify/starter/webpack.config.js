7da957557e9f96d6febbb1cf1f1f072b523ba972
azkzbf244 <63401856+azkzbf244@users.noreply.github.com>
2020-07-25 17:58:44 -0400

    message

--- 9-forkify/starter/webpack.config.js
+++ 9-forkify/starter/webpack.config.js
@@ -3,10 +3,11 @@ const path = require('path');
 module.exports = {
  entry: './src/js/index.js',
  output: {
- path: path.resolve(__dirname, 'dist'),
- filename: 'js/bundle.js'
+ path: path.resolve(__dirname, 'dist/js'),
+ filename: 'bundle.js'
 },
  devServer: {
-     contentBase: "./src",
- }
-};
+     contentBase: "./dist"
+    },
+ };
+
